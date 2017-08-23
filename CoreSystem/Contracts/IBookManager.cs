2a3d8cfeacd0fae74bcf0f831ca9713a827e309f
divastavisca <dagarwal@tavisca.com>
2017-08-23 13:45:34 +0530

    added definition for book manager

---
+++ CoreSystem/Contracts/IBookManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreSystem.Contracts
+{
+    public interface IBookManager
+    {
+        bool IsIssued(string id);
+        bool GetBookIdFromTitle(string bookTitle, out string id);
+        Book GetBookById(string id);
+    }
+}
